1bb0fc286c83056aec01c13eb5fa9e1cca1f4f1a
Akash Kumar <ak65469@gmail.com>
2023-05-05 03:10:27 +0530

    done

--- client/src/redux/action/adminAction.js
+++ client/src/redux/action/adminAction.js
@@ -7,8 +7,8 @@ import {
 } from '../actionTypes'
 
 // const url = "https://sore-teal-panther-sock.cyclic.app"
-const url ="https://ams-backend-ecru.vercel.app"
-const url1 = "https://ams-backend-ecru.vercel.app"
+const url ="https://easy-plum-katydid-toga.cyclic.app"
+const url1 = "https://easy-plum-katydid-toga.cyclic.app"
 
 const setAdmin = (data) => {
     return {
